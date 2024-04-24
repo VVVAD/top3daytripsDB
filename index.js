0219325c4883a0acda64c1a3291cc4459324370d
vvvad <Vadim.gavrilov@s.afeka.ac.il>
2024-04-24 03:54:33 +0300

    5

--- index.js
+++ index.js
@@ -10,13 +10,6 @@ const connectDB = require("./connectMongo");
 
 connectDB();
 
-const cors=require("cors");
-const corsOptions ={
-   origin:'*', 
-   credentials:true,            //access-control-allow-credentials:true
-   optionSuccessStatus:200,
-}
-
 app.use(cors(corsOptions)) // Use this after the variable declaration
 const UserModel = require("./models/user.model");
 const redis = require('./redis')
